f7b295de27784969e555e223734fd31678bb4e66
Sabbir <contbd@gmail.com>
2024-05-18 13:01:30 +0600

    Adding OOP.php

--- string.php
+++ string.php
@@ -1,5 +1,5 @@
 <?php
-$strings = ["Hello", "World", "PHP", "Programming", "Testing a longer string with more words"];
+$strings = ["Hello", "World", "PHP", "Programming"];
 
 foreach($strings as $string){
     $vowelCount = preg_match_all("/[aeiou]/i", $string);
